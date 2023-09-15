2d3cd853d0d4826b275bd235e5470f6e02a58db1
Udalov Pavel <udalovpavel95@gmail.com>
2023-09-16 00:19:58 +0500

    change port in url bot request

--- baggage.py
+++ baggage.py
@@ -1,23 +1,19 @@
-# import requests
-#
-# data_person = {
-#     'user_id': 12345,
-#     'name': 'test_user',
-#     'phone': '12345678'
-# }
-# data_note = {
-#     'user_id': 12345,
-#     'username': 'udalov',
-#     'note_type': 0,
-#     'text': 'afsdfjhdjaksd'
-# }
-#
-# url = 'http://localhost:8000/person/'
-# url = 'http://localhost:8000/note/'
-# response = requests.post(url=url, data=data_note)
-# print(response.status_code)
+import requests
 
-strrr = "/w asd"
-if strrr[2] == " ":
-    print(None)
-print(strrr[2])
+data_person = {
+    'user_id': 12345,
+    'name': 'test_user',
+    'phone': '12345678'
+}
+data_note = {
+    'user_id': 12345,
+    'username': 'udalov',
+    'note_type': 0,
+    'text': 'afsdfjhdjaksd'
+}
+
+url = 'http://localhost:8000/person/'
+url = 'http://212.109.197.194:80/note/'
+response = requests.post(url=url, data=data_note)
+print(response.status_code)
+
--- telebot/bot_1.py
+++ telebot/bot_1.py
@@ -45,7 +45,7 @@ async def add_note(message: types.Message):
     print("I see message")
     teg = message.text[:2]
     if teg.lower() in TEG_DICT and message.text[2] == " ":
-        url = 'http://localhost:80/note/'
+        url = 'http://212.109.197.194:80/note/'
         data = {
             'user_id': message.from_user.id,
             'username': message.from_user.username,
